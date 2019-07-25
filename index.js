06b81bebe458b40565dda8495acd401cc28f8373
user <19831942+Jamelle-Boose@users.noreply.github.com>
2019-07-24 22:37:06 -0700

    create server, root GET

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// implement your API here
+const express = require("express");
+const db = require("./data/db");
+
+const server = express();
+const port = 3000;
+
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.send(`Hello world from express!`);
+});
+
+// server.post('/users', (req, res) => {
+//   // /users {name : somename}
+//   const userInfo = req.body
+
+//   db.add(userInfo)
+// })
+
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
