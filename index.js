5ddfb3fca2fe845fb3d76b9d982b9c27cf172cc4
user <19831942+Jamelle-Boose@users.noreply.github.com>
2019-07-25 09:50:07 -0700

    create GET endpoint

--- index.js
+++ index.js
@@ -6,16 +6,61 @@ const port = 3000;
 
 server.use(express.json());
 
-server.get("/", (req, res) => {
-  res.send(`Hello world from express!`);
+/**
+| User Schema -------------------------------------
+| {
+  name: "Jane Doe", // String, required
+  bio: "Not Tarzan's Wife, another Jane",  // String
+  created_at: Mon Aug 14 2017 12:50:16 GMT-0700 (PDT) // Date, defaults to current date
+  updated_at: Mon Aug 14 2017 12:50:16 GMT-0700 (PDT) // Date, defaults to current date
+}
+|--------------------------------------------------
+*/
+
+/**
+|--------------------------------------------------
+|  GET    | /api/users     | Returns an array of all the user objects contained in the database.
+|--------------------------------------------------
+*/
+
+server.get("/users", (request, response) => {
+  db.find()
+    .then(users => {
+      response.status(200).json(users);
+    })
+    .catch(err => {
+      response.status(500).json({
+        success: false,
+        err
+      });
+    });
 });
 
-// server.post('/users', (req, res) => {
-//   // /users {name : somename}
-//   const userInfo = req.body
+/**
+|--------------------------------------------------
+| | POST   | /api/users     | Creates a user using the information sent inside the `request body`.
+|--------------------------------------------------
 
-//   db.add(userInfo)
-// })
+**/
+
+server.post("/users", (request, response) => {
+  // /users {name : somename}
+  const { body } = request;
+
+  db.insert(body)
+    .then(user => {
+      response.status(201).json({
+        success: true,
+        user
+      });
+    })
+    .catch(err => {
+      response.status(500).json({
+        success: false,
+        err
+      });
+    });
+});
 
 server.listen(port, () => {
   console.log(`Server listening on port ${port}`);
