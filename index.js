0d043fe316dd22d149067cc6736afa9164bca6c4
Jamelle-Boose <19831942+Jamelle-Boose@users.noreply.github.com>
2019-07-27 11:34:32 -0700

    add /api to endpoints

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ server.use(express.json());
 |--------------------------------------------------
 */
 
-server.get("/users", (request, response) => {
+server.get("/api/users", (request, response) => {
   db.find()
     .then(users => {
       response.status(200).json(users);
@@ -32,20 +32,26 @@ server.get("/users", (request, response) => {
 
 **/
 
-server.post("/users", (request, response) => {
-  // /users {name : somename}
+server.post("/api/users", (request, response) => {
   const { body } = request;
+  const { name, bio } = body;
 
   db.insert(body)
     .then(user => {
-      response.status(201).json({
-        success: true,
-        user
-      });
+      console.log({ name, bio });
+      if (name && bio) {
+        response.status(400).json({
+          errorMessage: "Please provide name and bio for the user."
+        });
+      } //else {
+      //   response.status(201).json({
+      //     user
+      //   });
+      // }
     })
     .catch(err => {
       response.status(500).json({
-        success: false,
+        error: "There was an error while saving the user to the database",
         err
       });
     });
@@ -60,10 +66,70 @@ server.post("/users", (request, response) => {
 server.get("/users/:id", (request, response) => {
   const { id } = request.params;
 
-  db.find(id)
+  db.findById(id)
     .then(user => {
-      // console.log(user[id - 1]);
-      response.status(200).json(user[id - 1]);
+      //try to refactor and send a message if user id doesn't exist
+      response.status(200).json(user);
+    })
+    .catch(err => {
+      response.status(500).json({
+        success: false,
+        err
+      });
+    });
+});
+
+/**
+|--------------------------------------------------
+|  DELETE | /api/users/:id | Removes the user with the specified `id` and returns the deleted user.
+|--------------------------------------------------
+*/
+
+server.delete("/api/users/:id", (request, response) => {
+  const { id } = request.params;
+
+  db.remove(id)
+    .then(del => {
+      if (del) {
+        response.status(204).end();
+      } else {
+        response.status(404).json({
+          success: false,
+          message: "I cannot find the user you are looking for, Dave."
+        });
+      }
+    })
+    .catch(err => {
+      response.status(500).json({
+        success: false,
+        err
+      });
+    });
+});
+
+/**
+|--------------------------------------------------
+| PUT    | /api/users/:id | Updates the user with the specified `id` using data from the `request body`. Returns the modified document, **NOT the original**.
+|--------------------------------------------------
+*/
+
+server.put("/api/users/:id", (request, response) => {
+  const { id } = request.params;
+  const userInfo = request.body;
+
+  db.update(id, userInfo)
+    .then(updated => {
+      if (updated) {
+        response.status(200).json({
+          success: true,
+          updated
+        });
+      } else {
+        response.status(404).json({
+          success: false,
+          messsage: "I cannot the user you are looking for, Dave."
+        });
+      }
     })
     .catch(err => {
       response.status(500).json({
